747868cd38832e72a83fe6c26007a062848528a0
Fran Romero <fromero@sevensols.com>
2022-11-15 03:38:49 +0100

    Modified "Ejercicio2.py" in order to use the classes previously created.

--- Ejercicio2/Ejercicio2.py
+++ Ejercicio2/Ejercicio2.py
@@ -1,24 +1,40 @@
 #!/usr/bin/env python
+
+#imports
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-driver = webdriver.Firefox()
+from pages.homepage import Homepage
+from pages.wikipedia import Wikipedia
 
-print("Finding \"automatización\" in Google...\n")
-keyword = "automatización"
-driver.get("https://google.es/search?q="+keyword)
-driver.find_element("id","L2AGLb").click()  #Accept cookies in Google
+
+##Constants and variables
+GOOGLE_URL = "https://google.es/search?q="
+KEYWORD = "automatización"
+TEXT_TO_FIND = 'primer proceso'
+driver = webdriver.Firefox()
+screenshot_name = 'screenshot.png'
+partial_screenshot_name = 'paragraph_screenshot.png'
+
+
+##Program execution
+print("Finding \"" + KEYWORD + "\" in Google...\n")
+driver.get(GOOGLE_URL + KEYWORD)
+google = Homepage(driver)
+google.accept_cookies()
 
 print("Selecting the Wikipedia link...\n")
-wikipedia = driver.find_element(By.PARTIAL_LINK_TEXT, 'Wikipedia').get_attribute("href")
-driver.get(wikipedia)
+wikipedia_link = google.get_wikipedia_link()
+driver.get(wikipedia_link)
+wikipedia = Wikipedia(driver)
 
 print("Checking in which year the first automatic process took place...")
-elements = driver.find_elements(By.TAG_NAME, 'p')
-for e in elements:
-    text = e.text
-    if 'primer proceso' in text:
-        print(text)
+paragraph = wikipedia.find_paragraph_by_text(TEXT_TO_FIND)
+print(paragraph.text)
 
 print("\nTaking a screenshot of the Wikipedia's page...")
-driver.save_screenshot('screenshot.png')
+wikipedia.screenshot(screenshot_name)
+
+print("\nTaking a screenshot of the selected paragraph...")
+wikipedia.partial_screenshot(TEXT_TO_FIND, partial_screenshot_name)
+
 driver.close()
